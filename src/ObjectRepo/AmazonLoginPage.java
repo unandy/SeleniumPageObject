e70f2ab69138b10a68d697ca580cd425e2fa4e06
unandy <upali1985@gmail.com>
2016-02-10 09:04:12 -0500

    add cart commit

--- src/ObjectRepo/AmazonLoginPage.java
+++ src/ObjectRepo/AmazonLoginPage.java
@@ -3,23 +3,28 @@ package ObjectRepo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AmazonLoginPage {
 	WebDriver driver;
+	WebDriverWait dynamicwait;
+	By signin = By.id("nav-link-yourAccount");
+	By email = By.id("ap_email");
+	By password = By.id("ap_password");
+	By go = By.id("signInSubmit");
 	
-	public AmazonLoginPage(WebDriver driver){
-		this.driver = driver;	
+	public AmazonLoginPage(WebDriver driver, WebDriverWait dynamicwait){
+		this.driver = driver;
+		this.dynamicwait = dynamicwait;
 	}
-		By signin = By.id("nav-link-yourAccount");
-		By email = By.id("ap_email");
-		By password = By.id("ap_password");
-		By go = By.id("signInSubmit");
-		
+			
 	public WebElement SignInLink(){
 			return driver.findElement(signin);
 		}
 		
 	public WebElement EmailId(){
+		dynamicwait.until(ExpectedConditions.presenceOfElementLocated(email));
 		return driver.findElement(email);
 	}
 	
